2219586cb485ad4b4c3f1bbe1a2e83569cc57b37
Vladimir Adamic <vladimir.adamic@gmail.com>
2026-02-11 12:41:45 +0100

    fix(app): use relative path in vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -4,7 +4,7 @@ import viteFastify from "@fastify/vite/plugin";
 
 export default {
   root: resolve(import.meta.dirname, "src", "client"),
-  plugins: [viteFastify({ spa: true }), viteReact()],
+  plugins: [viteFastify({ spa: true, useRelativePaths: true }), viteReact()],
   build: {
     emptyOutDir: true,
     outDir: resolve(import.meta.dirname, "dist", "client"),
