0e782d549fadf3950dc08d56bdba2934d9896a88
Vladimir Adamic <vladimir.adamic@gmail.com>
2026-03-16 11:21:04 +0100

    fix(harness): skip running non vitest examples

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    exclude: [
+      "**/node_modules/**",
+      "**/packages/otel-testing-harness/examples/jest/**",
+      "**/packages/otel-testing-harness/examples/tap/**",
+    ],
   },
 });
